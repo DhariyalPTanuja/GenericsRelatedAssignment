369e30a2bfa2098fd9520b24a6bd311f55f43996
DhariyalPTanuja <dhariyal.ptanuja@gmail.com>
2022-10-03 18:11:30 +0530

    find maximum String position

--- NumberMaximumPosition.java
+++ NumberMaximumPosition.java
@@ -5,6 +5,9 @@ import java.util.Scanner;
 public class NumberMaximumPosition {
     private static int inum1, inum2, inum3, intMaximumNumber;
     private static float fnum1, fnum2, fnum3, floatMaximumNumber;
+    private static String snum1, snum2, snum3, stringMaximumNumber;
+
+
     public static void integerNumberMax(){
         Scanner scanobj = new Scanner(System.in);
         System.out.println(" enter the int number n1 : ");
@@ -47,8 +50,34 @@ public class NumberMaximumPosition {
         }
 
     }
+    public static void stringNumberMax(){
+        Scanner scanstringobj = new Scanner(System.in);
+        System.out.println(" enter the String  s1 : ");
+        snum1 = scanstringobj.next();
+        System.out.println(" enter the String s2 : ");
+        snum2 = scanstringobj.next();
+        System.out.println(" enter the String s3 : ");
+        snum3 = scanstringobj.next();
+        int c11 = snum1.compareTo(snum2);
+        int c12 = snum1.compareTo(snum3);
+        int c21 = snum2.compareTo(snum3);
+
+
+        if( c11 > 0 && c12 > 0){
+            stringMaximumNumber = snum1;
+            System.out.println( "Maximum String at 1st Position :" + snum1);
+        } else if(c11 < 0 && c21 > 0){
+            stringMaximumNumber = snum2;
+            System.out.println("Maximum String at 2nd Position : " + snum2);
+        } else {
+            stringMaximumNumber = snum3;
+            System.out.println("Maximum number in 3rd Position : " + snum3);
+        }
+
+    }
     public static void main(String[] args) {
-       // integerNumberMax();
-        floatNumberMax();
+        integerNumberMax();
+       floatNumberMax();
+        stringNumberMax();
     }
 }
